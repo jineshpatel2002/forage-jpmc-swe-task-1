8ebb09adcc20b7bb11638a67aea13477f733869b
jineshpatel2002 <jineshpatel2002@gmail.com>
2024-08-30 18:12:46 +0530

    add test cases

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for qoute in quotes:
+      self.assertEqual(getDataPoint(qoute), (qoute['stock'], qoute['top_bid']['price'], qoute['top_ask']['price'], (qoute['top_bid']['price'] + qoute['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +17,22 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for qoute in quotes:
+      self.assertGreater(qoute['top_bid']['price'], qoute['top_ask']['price'])
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getDataPoint_calculatePriceWithEmptyQuote(self):
+    quote = {}
+    self.assertEqual(getDataPoint(quote), (None, None, None, None))
 
+  def test_getDataPoint_calculatePriceWithMissingKeys(self):
+    quote = {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453'}
+    self.assertEqual(getDataPoint(quote), (None, None, None, None))
+
+  def test_getDataPoint_calculatePriceWithZeroAskPrice(self):
+    quote = {'top_ask': {'price': 0, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'}
+    self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], None))
 
 
 if __name__ == '__main__':
